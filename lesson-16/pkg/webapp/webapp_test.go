996795cfa21ec681e8fe43510c6470f50eaf21d5
Beglov Sergey <beglov.sergey@yandex.ru>
2021-09-16 19:39:04 +0300

    Lesson 16 - Copy files from lesson-15

---
+++ lesson-16/pkg/webapp/webapp_test.go
@@ -0,0 +1,48 @@
+package webapp
+
+import (
+	"go-core-lessons/lesson-16/pkg/engine"
+	"go-core-lessons/lesson-16/pkg/index/hash"
+	"go-core-lessons/lesson-16/pkg/storage/btree"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+var srv *Service
+
+func TestMain(m *testing.M) {
+	srv := New(":8000", engine.New(hash.New(), btree.New()))
+	srv.endpoints()
+
+	os.Exit(m.Run())
+}
+
+func TestService_indexHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/index", nil)
+
+	rr := httptest.NewRecorder()
+
+	http.DefaultServeMux.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("код неверен: получили %d, а хотели %d", rr.Code, http.StatusOK)
+	}
+
+	t.Log("Response: ", rr.Body)
+}
+
+func TestService_docsHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/docs", nil)
+
+	rr := httptest.NewRecorder()
+
+	http.DefaultServeMux.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("код неверен: получили %d, а хотели %d", rr.Code, http.StatusOK)
+	}
+
+	t.Log("Response: ", rr.Body)
+}
